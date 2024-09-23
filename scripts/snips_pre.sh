3f50cdade13d62ce4226a0aa31fd568fd2c4af46
gkruddl <124562401+gkruddl@users.noreply.github.com>
2024-09-23 00:41:11 -0400

    Update snips_pre.sh

--- scripts/snips_pre.sh
+++ scripts/snips_pre.sh
@@ -1,14 +1,14 @@
 CUDA_VISIBLE_DEVICES=0 python extract_full_features.py \
-    --dataset snips \
-    --ood_datasets snips_ood \
-    --output_dir output/snips/pre \
+    --dataset clinc \
+    --ood_datasets clinc_ood \
+    --output_dir output/clinc/pre \
     --model roberta-base 
 
 CUDA_VISIBLE_DEVICES=0 python ced_data.py \
-    --dataset snips \
-    --ood_datasets snips_ood \
-    --input_dir output/snips/pre \
-    --output_dir output/snips/pre/CED\
+    --dataset clinc \
+    --ood_datasets clinc_ood \
+    --input_dir output/clinc/pre \
+    --output_dir output/clinc/pre/CED\
     --oracle_num 3\
     --auxiliary_num 5\
     --bound 100\
@@ -16,16 +16,35 @@ CUDA_VISIBLE_DEVICES=0 python ced_data.py \
     --token_pooling cls
 
 CUDA_VISIBLE_DEVICES=0 python ced_features.py \
-    --dataset snips \
-    --ood_datasets snips_ood \
-    --output_dir output/snips/pre/CED \
+    --dataset clinc \
+    --ood_datasets clinc_ood \
+    --output_dir output/clinc/pre/CED \
     --model roberta-base 
 
 CUDA_VISIBLE_DEVICES=0 python md_eval.py \
-    --dataset snips \
-    --ood_datasets snips_ood \
-    --base_dir output/snips/pre \
-    --input_dir output/snips/pre/CED/features \
+    --dataset clinc \
+    --ood_datasets clinc_ood \
+    --base_dir output/clinc/pre \
+    --input_dir output/clinc/pre/CED/features \
     --layer_pooling last \
     --token_pooling cls \
+    --seed pre
+    
+CUDA_VISIBLE_DEVICES=0 python knn_eval.py \
+        --dataset clinc \
+        --ood_datasets clinc_ood \
+        --base_dir output/clinc/pre \
+        --input_dir output/clinc/pre/CED/features \
+        --layer_pooling last \
+        --token_pooling cls \
+        --seed pre
+
+CUDA_VISIBLE_DEVICES=0 python flats_eval.py \
+    --dataset clinc \
+    --ood_datasets clinc_ood \
+    --base_dir output/clinc/pre \
+    --input_dir output/clinc/pre/CED/features \
+    --layer_pooling last \
+    --token_pooling cls \
+    --model roberta-base \
     --seed pre
