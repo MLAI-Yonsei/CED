b6056a6d6cf313b8be6903ca324adb19df2bba6a
gkruddl <124562401+gkruddl@users.noreply.github.com>
2024-09-23 00:44:13 -0400

    Update clinc_train.sh

--- scripts/clinc_train.sh
+++ scripts/clinc_train.sh
@@ -16,20 +16,7 @@ do
         --output_dir output/clinc/$seed \
         --model roberta-base \
         --pretrained_model output/clinc/$seed/model.pt
-
-    CUDA_VISIBLE_DEVICES=0 python ood_test_embedding.py \
-        --dataset clinc \
-        --ood_datasets clinc_ood \
-        --input_dir output/clinc/$seed \
-        --token_pooling cls \
-        --layer_pooling last
-
-    CUDA_VISIBLE_DEVICES=0 python ood_test_embedding.py\
-        --dataset clinc\
-        --ood_datasets clinc_ood \
-        --input_dir output/clinc/$seed \
-        --token_pooling avg \
-        --layer_pooling avg
+        
 done
 
 
